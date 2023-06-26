88534e283c67d82d0263c24cb76a3980acaf7d15
pepo999 <pietroviglino999@gmail.com>
2023-06-26 08:27:33 +0200

    update

--- src/app/components/dinner/dinner.component.ts
+++ src/app/components/dinner/dinner.component.ts
@@ -1,33 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-dinner',
   templateUrl: './dinner.component.html',
   styleUrls: ['./dinner.component.scss']
 })
-export class DinnerComponent {
+export class MovieComponent implements OnInit {
+  @Input() dinnerList?: any[];
 
-  dinners: any = [
-    {
-      "title": "Insalatona di riso 2.0",
-      "watched": false
-    },
-    {
-      "title": "Insalatona di riso 2.0",
-      "watched": false
-    },
-    {
-      "title": "Insalatona di riso 2.0",
-      "watched": false
-    },
-    {
-      "title": "Insalatona di riso 2.0",
-      "watched": false
-    },
-    {
-      "title": "Insalatona di riso 2.0",
-      "watched": true
-    },
-  ]
+  randomDinner?: any;
 
+  constructor(public dataService: DataService) { }
+
+  ngOnInit() {
+    this.fetchDinner();
+  }
+
+  fetchDinner() {
+    this.dataService.getDinnersAndMovies().subscribe(
+      (response) => {
+        this.dinnerList = response.filter((item) => item.dinner.title);
+      },
+      (error) => {
+        console.error('Error fetching dinners:', error);
+      }
+    );
+  }
+
+  getRandomDinner() {
+    const randomIndex = Math.floor(Math.random() * this.dinnerList!.length);
+    this.randomDinner = this.dinnerList![randomIndex];
+  }
+
+  deleteDinner(movie: any) {
+    this.dataService.delete(movie).subscribe(
+      () => {
+        this.fetchDinner();
+      },
+      (error) => {
+        // Handle error here
+        console.error('Error deleting movie:', error);
+      }
+    );
+  }
 }
